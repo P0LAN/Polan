a715fe5e0f5a8b2e165d0139bc067dc555c1a0d0
Polan-foundation <0polan@protonmail.com>
2022-12-25 19:58:26 +0300

    big ui change with 3 commands

--- Polan/Form1.cs
+++ Polan/Form1.cs
@@ -2,20 +2,23 @@ using System.Speech.Synthesis;
 using System.Speech.Recognition;
 using System.Diagnostics;
 using System;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
 
 namespace Polan
 {
-    public partial class Form1 : Form
+    public partial class Polan : MaterialForm
     {
         SpeechSynthesizer s = new SpeechSynthesizer();
         Boolean wake = true;
 
         Choices list = new Choices();
-        public Form1()
+        public Polan()
         {
             SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
 
-            list.Add(new String[] { "hello", "how are you", "what time is it", "what is today", "what is the time", "open google", "open github", "open your website", "go to downloads page for polan", "wake", "sleep" });
+            list.Add(new String[] { "hello", "how are you", "what time is it", "what is today", "what is the time", "open google", "open github", "open your website", "go to downloads page for polan", "wake", "sleep", "restart", "update" });
 
             Grammar gr  = new Grammar(new GrammarBuilder(list));
 
@@ -30,12 +33,22 @@ namespace Polan
             catch { return; }
 
 
-
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.Grey900, Primary.Grey500, Accent.LightBlue200, TextShade.WHITE);
 
             s.SelectVoiceByHints(VoiceGender.Male);
             s.Speak("polan starting up");
 
             InitializeComponent();
+        }
+        /// test public void down
+       
+        public void restart()
+        {
+            Process.Start(@"C:\Users\ttt\polan.exe");
+            Environment.Exit(0);    
         }
 
         public void say(String h)
