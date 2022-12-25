8beb508625ed89838184a84588540c37bcdac441
Polan-foundation <0polan@protonmail.com>
2022-12-25 20:00:09 +0300

    fixed some issues #!

--- Polan/Form1.Designer.cs
+++ Polan/Form1.Designer.cs
@@ -30,99 +30,12 @@ namespace Polan
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Polan));
-            this.materialContextMenuStrip1 = new MaterialSkin.Controls.MaterialContextMenuStrip();
-            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.materialContextMenuStrip2 = new MaterialSkin.Controls.MaterialContextMenuStrip();
-            this.fToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.fToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.textBoxContextMenuStrip1 = new MaterialSkin.Controls.TextBoxContextMenuStrip();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.dsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.Main = new System.Windows.Forms.TabPage();
             this.Settings = new System.Windows.Forms.TabPage();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.materialContextMenuStrip1.SuspendLayout();
-            this.materialContextMenuStrip2.SuspendLayout();
-            this.contextMenuStrip1.SuspendLayout();
             this.materialTabControl1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // materialContextMenuStrip1
-            // 
-            this.materialContextMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.materialContextMenuStrip1.Depth = 0;
-            this.materialContextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.homeToolStripMenuItem,
-            this.sToolStripMenuItem});
-            this.materialContextMenuStrip1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialContextMenuStrip1.Name = "materialContextMenuStrip1";
-            this.materialContextMenuStrip1.Size = new System.Drawing.Size(106, 48);
-            // 
-            // homeToolStripMenuItem
-            // 
-            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
-            this.homeToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
-            this.homeToolStripMenuItem.Text = "home";
-            // 
-            // sToolStripMenuItem
-            // 
-            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
-            this.sToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
-            this.sToolStripMenuItem.Text = "s";
-            // 
-            // materialContextMenuStrip2
-            // 
-            this.materialContextMenuStrip2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.materialContextMenuStrip2.Depth = 0;
-            this.materialContextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fToolStripMenuItem,
-            this.fToolStripMenuItem1});
-            this.materialContextMenuStrip2.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialContextMenuStrip2.Name = "materialContextMenuStrip2";
-            this.materialContextMenuStrip2.Size = new System.Drawing.Size(79, 48);
-            // 
-            // fToolStripMenuItem
-            // 
-            this.fToolStripMenuItem.Name = "fToolStripMenuItem";
-            this.fToolStripMenuItem.Size = new System.Drawing.Size(78, 22);
-            this.fToolStripMenuItem.Text = "f";
-            // 
-            // fToolStripMenuItem1
-            // 
-            this.fToolStripMenuItem1.Name = "fToolStripMenuItem1";
-            this.fToolStripMenuItem1.Size = new System.Drawing.Size(78, 22);
-            this.fToolStripMenuItem1.Text = "f";
-            // 
-            // textBoxContextMenuStrip1
-            // 
-            this.textBoxContextMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.textBoxContextMenuStrip1.Depth = 0;
-            this.textBoxContextMenuStrip1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.textBoxContextMenuStrip1.Name = "textBoxContextMenuStrip1";
-            this.textBoxContextMenuStrip1.Size = new System.Drawing.Size(123, 160);
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.dsToolStripMenuItem,
-            this.sToolStripMenuItem1});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(87, 48);
-            // 
-            // dsToolStripMenuItem
-            // 
-            this.dsToolStripMenuItem.Name = "dsToolStripMenuItem";
-            this.dsToolStripMenuItem.Size = new System.Drawing.Size(86, 22);
-            this.dsToolStripMenuItem.Text = "ds";
-            // 
-            // sToolStripMenuItem1
-            // 
-            this.sToolStripMenuItem1.Name = "sToolStripMenuItem1";
-            this.sToolStripMenuItem1.Size = new System.Drawing.Size(86, 22);
-            this.sToolStripMenuItem1.Text = "s";
             // 
             // materialTabControl1
             // 
@@ -176,28 +89,16 @@ namespace Polan
             this.Controls.Add(this.materialTabControl1);
             this.DrawerShowIconsWhenHidden = true;
             this.DrawerTabControl = this.materialTabControl1;
+            this.DrawerUseColors = true;
             this.Name = "Polan";
             this.Text = "Polan";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.materialContextMenuStrip1.ResumeLayout(false);
-            this.materialContextMenuStrip2.ResumeLayout(false);
-            this.contextMenuStrip1.ResumeLayout(false);
             this.materialTabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private MaterialSkin.Controls.MaterialContextMenuStrip materialContextMenuStrip1;
-        private ToolStripMenuItem homeToolStripMenuItem;
-        private ToolStripMenuItem sToolStripMenuItem;
-        private MaterialSkin.Controls.MaterialContextMenuStrip materialContextMenuStrip2;
-        private ToolStripMenuItem fToolStripMenuItem;
-        private ToolStripMenuItem fToolStripMenuItem1;
-        private MaterialSkin.Controls.TextBoxContextMenuStrip textBoxContextMenuStrip1;
-        private ContextMenuStrip contextMenuStrip1;
-        private ToolStripMenuItem dsToolStripMenuItem;
-        private ToolStripMenuItem sToolStripMenuItem1;
         private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
         private TabPage Main;
         private TabPage Settings;
